5e3969e7a2f07182005be2e9adc709880323af49
ioancallay <ivanar71@uniandes.edu.ec>
2024-08-01 23:34:33 -0500

    #Crear la clase para la conexión con la base de datos

---
+++ 02getpopst/config/conexion.php
@@ -0,0 +1,25 @@
+<?php
+
+class ClaseConectar
+{
+    public $conexion;
+    protected $db;
+    private $db_user = "root";
+    private $db_password = "";
+    private $db_host = "localhost";
+    private $db_name = "sexto";
+
+    public function ProcedimientoConectar()
+    {
+        $this->conexion = mysqli_connect($this->db_host, $this->db_user, $this->db_password, $this->db_name);
+        mysqli_query($this->conexion, "SET NAMES 'utf8'");
+        if (!$this->conexion->connect_error) {
+            die("No se pudo conectar al servidor: " . $this->conexion->connect_error);
+        }
+        $this->db = $this->conexion;
+        if ($this->db == 0) {
+            die("No se pudo conectar a la base de datos" . $this->conexion->connect_error);
+        }
+        return $this->conexion;
+    }
+}
